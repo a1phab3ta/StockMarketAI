2fae1f5fa050f20efb3e7a0557e9f5eb4a64fabf
a1phab3ta <150404455+a1phab3ta@users.noreply.github.com>
2024-06-21 14:47:24 -0400

    Added GUI files
    Added the files for all the pages, no functionality except moving through them yet, though

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/refresh_model')
+def refresh_model():
+    return render_template('refresh_model.html')
+
+@app.route('/current_portfolio')
+def current_portfolio():
+    return render_template('current_portfolio.html')
+
+@app.route('/top_bearish')
+def top_bearish():
+    return render_template('top_bearish.html')
+
+@app.route('/top_bullish')
+def top_bullish():
+    return render_template('top_bullish.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

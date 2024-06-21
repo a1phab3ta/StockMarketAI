d90295ba15846075277e69b695070508dd6aceab
a1phab3ta <150404455+a1phab3ta@users.noreply.github.com>
2024-06-21 15:51:41 -0400

    Load stock tickers
    Loaded stock tickers dynamically from website

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+
+def get_stock_tickers():
+    url = 'https://stockanalysis.com/stocks/'  # Replace with the actual URL
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    tickers = []
+    for td in soup.find_all('td', class_='svelte-1yyv6eq'):
+        # Check if the <td> contains an <a> tag
+        a_tag = td.find('a')
+        if a_tag:
+            tickers.append(a_tag.text.strip())
+
+    return tickers
 
 @app.route('/')
 def main():
@@ -21,6 +37,11 @@ def top_bearish():
 @app.route('/top_bullish')
 def top_bullish():
     return render_template('top_bullish.html')
+
+@app.route('/stock_tickers')
+def stock_tickers():
+    tickers = get_stock_tickers()
+    return render_template('stock_tickers.html', tickers=tickers)
 
 if __name__ == '__main__':
     app.run(debug=True)
